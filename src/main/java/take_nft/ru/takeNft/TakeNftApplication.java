5579b4af29a4e3b1997dceb2fcff16b34d5a1ea2
GisDuck <sigaj04@mail.ru>
2025-05-08 17:29:52 +0300

    1

---
+++ src/main/java/take_nft/ru/takeNft/TakeNftApplication.java
@@ -0,0 +1,13 @@
+package take_nft.ru.takeNft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeNftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeNftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/take_nft/ru/takeNft/TakeNftApplicationTests.java
@@ -0,0 +1,13 @@
+package take_nft.ru.takeNft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeNftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
