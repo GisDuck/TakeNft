e5608464ff77e751184525d988634c4016355930
GisDuck <sigaj04@mail.ru>
2025-05-27 04:02:32 +0300

    infoController log

--- src/main/java/take_nft/ru/takeNft/controller/InfoController.java
+++ src/main/java/take_nft/ru/takeNft/controller/InfoController.java
@@ -1,16 +1,29 @@
 package take_nft.ru.takeNft.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Slf4j
+@Controller
 public class InfoController {
 
-    @GetMapping("/player/")
-    public String index() {
+    @GetMapping("/player/{id}")
+    public String player(@PathVariable String id) {
+        log.info("словили player c id: {}", id);
         return "forward:/pages/index.html";
     }
 
+    @GetMapping("/nft/{id}")
+    public String nft(@PathVariable String id) {
+        log.info("словили nft c id: {}", id);
+        return "forward:/pages/index.html";
+    }
+
+    @GetMapping("/collection/{id}")
+    public String collection(@PathVariable String id) {
+        log.info("словили collection c id: {}", id);
+        return "forward:/pages/index.html";
+    }
 }
