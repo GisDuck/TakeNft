978ab7bff9348916a208f466bbe933569887cd34
GisDuck <sigaj04@mail.ru>
2025-06-10 13:50:25 +0300

    fff

--- src/main/java/take_nft/ru/takeNft/service/TonApiService.java
+++ src/main/java/take_nft/ru/takeNft/service/TonApiService.java
@@ -1,6 +1,7 @@
 package take_nft.ru.takeNft.service;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.stereotype.Service;
 import org.ton.schema.nft.NftItem;
 import org.ton.schema.tonconnect.AccountInfoByStateInit;
 import org.ton.tonapi.sync.Tonapi;
@@ -8,6 +9,7 @@ import take_nft.ru.takeNft.dto.TonProofRequest;
 
 import java.util.List;
 
+@Service
 public class TonApiService {
 
     private static Dotenv dotenv = Dotenv.load();
